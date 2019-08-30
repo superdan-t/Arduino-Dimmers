625080d2f65c3cec28b282e3ec5c59f8697159c8
Dan T <superdan.tier@gmail.com>
2019-08-30 15:42:29 -0400

    Restarted with new design

--- DimmerControl.cpp
+++
@@ -1,8 +0,0 @@
-#include "Dimmers.h"
-#include "Arduino.h"
-
-void DimmerControlMaster::initialize(dimmer_t *dim) {
-	pinMode(dim->_pin, OUTPUT);
-	//setIntensity(&dim, 0);
-	dim->__initialized = true;
-}
--- Dimmers.h
+++ Dimmers.h
@@ -1,78 +1,61 @@
-/*
-	DimmerControlMaster by Daniel Tierney
-	Provides functions, special effects, and controls for outputs at an abstracted level
-*/
-
-#ifndef DimmerControlMaster_h
-#define DimmerControlMaster_h
+#ifndef Dimmers_h
+#define Dimmers_h
 
 #include "Arduino.h"
 
-typedef struct dimmer_t {
-	uint8_t _pin;
-	uint8_t _method = 0;
-	uint8_t _intensity = 0;
-	uint8_t _function = 0;
-	uint8_t _data[5];
-	uint8_t _enabled = false;
-	uint8_t _bipolar = false;
-	uint8_t _inverse = false;
-	uint8_t __initialized = false;
-} Dimmer;
-
-class DimmerControlMaster {
+class Dimmer {
 	public:
-		DimmerControlMaster();
-		
+	
 		//	Control
 		
-		void initialize(dimmer_t *dim);
+		void begin();
+		void end();
 		
-		void update(dimmer_t *dim);
-		void setIntensity(dimmer_t *dim, uint8_t intensity);
+		//	Operation
 		
-		//	Input
+		void setIntensity(uint8_t intensity);
+		void setFunction(uint8_t function);
+		void setFunctionData(uint8_t data, uint8_t place);
+		void setFunctionData(uint8_t data[]);
+		void clearFunction();
+		void run();
+		void run(bool force);
 		
-		static void dimmerReceiveEvent(int16_t amount);
-		static void dimmerRequestEvent();
+		//	Set properties
 		
-		//	Output
+		void enable();
+		void disable();
+		void setBipolar();
+		void setDimming();
+		void setInverse();
+		void setDirect();
+		void setNextInSequence(Dimmer *dim);
 		
-		void enableWireMethod(uint8_t address);
-		void disableWireMethod();
+		//	Get Properties
 		
-		//	Get properties of dimmer
-		
-		uint8_t pin(dimmer_t *dim);
-		uint8_t remoteID(dimmer_t *dim);
-		uint8_t method(dimmer_t *dim);
-		uint8_t intensity(dimmer_t *dim);
-		uint8_t function(dimmer_t *dim);
-		uint8_t data(dimmer_t *dim, uint8_t index);
-		uint8_t data(dimmer_t *dim, uint8_t *arrayStart);
-		uint8_t isEnabled(dimmer_t *dim);
-		uint8_t isBipolar(dimmer_t *dim);
-		uint8_t isInverse(dimmer_t *dim);
-		
-		//	Set properties of dimmer
-		
-		void setPin(dimmer_t *dim, uint8_t pin);
-		void setRemoteID(dimmer_t *dim, uint8_t id);
-		void setMethod(dimmer_t *dim, uint8_t method);
-		void setFunction(dimmer_t *dim, uint8_t function);
-		void setData(dimmer_t *dim, uint8_t index, uint8_t data);
-		void setData(dimmer_t *dim, uint8_t *data);
-		void enable(dimmer_t *dim);
-		void disable(dimmer_t *dim);
-		void bipolar(dimmer_t *dim);
-		void dimming(dimmer_t *dim);
-		void inverse(dimmer_t *dim);
-		void direct(dimmer_t *dim);
-		
+		bool enabled();
+		bool disabled();
+		bool bipolar();
+		bool dimming();
+		bool inverse();
+		bool direct();
+		Dimmer *nextInSequence();
+	
 	private:
-		uint8_t _tickDelay;
-		uint8_t _wireEnabled;
-		uint64_t _nextTick;
+	
+		//	Variables
+	
+		uint8_t _pin;
+		uint8_t _method;
+		uint8_t _intensity;
+		uint8_t _function;
+		uint8_t _functionData[5];
+		bool _enabled;
+		bool _bipolar;
+		bool _inverse;
+		bool _initialized;
+		Dimmer *_nextInSequence;
+		
 };
 
 #endif
