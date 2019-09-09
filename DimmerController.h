8dde573a1f6a021112cad3f9bb9946c6bd1e1df4
Dan T <cat101126@gmail.com>
2019-09-09 00:19:44 -0400

    More DimmerControl capabilities

--- DimmerController.h
+++ DimmerController.h
@@ -12,12 +12,15 @@ class DimmerController {
 		void onEnd( void(*f)(Dimmer) );
 		void onIntensityChanged( void(*f)(Dimmer) );
 		void onEffectChanged( void(*f)(uint8_t, Dimmer) );
-		void checkPinValidity( bool(*f)(uint8_t) );
+		void onEffectDataChanged( void(*f)(uint8_t, uint8_t, Dimmer) );
+		void onPropChanged( void(*f)(uint8_t, uint8_t, Dimmer) );
+		void onCheckPinValidity( bool(*f)(uint8_t) );
 		void (*_beginFunc)(Dimmer);
 		void (*_endFunc)(Dimmer);
 		void (*_intensityFunc)(Dimmer);
 		void (*_effectFunc)(uint8_t, Dimmer);
-		bool (*_pinFunc)(uint8_t);
+		void (*_dataFunc)(uint8_t, uint8_t, Dimmer);
+		uint8_t (*_pinFunc)(uint8_t);
 	private:
 	
 };
